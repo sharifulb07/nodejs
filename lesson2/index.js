004f6c8c65b995aa2dddf3b9d0c6bf9430d9448d
shariful islam <sharifulb07@gmail.com>
2022-04-07 00:14:46 -0700

    Basic structure of nodejs application

--- lesson2/index.js
+++ lesson2/index.js
@@ -1,13 +1,48 @@
-const fs=require('fs');
-const ourReadStream=fs.createReadStream(`${__dirname}/bigdata.txt`, 'utf8');
-const ourWriteStream=fs.createWriteStream(`${__dirname}/output.txt`);
+/*
+Title:Basic node app example
+Description:Simple node application thaht print random quotes per second
+Author:Shariful islam(by following Learn with sumit)
+Date: 07/04/2022
 
-ourReadStream.pipe(ourWriteStream);
 
-// ourReadStream.on('data',(chunk)=>{
-//     ourWriteStream.write(chunk);
-    
+*/
 
-// } );
+// Dependencies
+const mathLibrary=require('../lib/math');
+const quotesLibrary=require('../lib/quotes');
 
-console.log('Hello');
+// app object - Module scaffolding
+const app={};
+
+// configuration
+app.config={
+    timeBetweenQuotes:1000,
+};
+
+app.printAQuote=function printAQuote(){
+
+    //get all the quotes
+    const allQuotes=quotesLibrary.allQuotes();
+    // get the length of the quotes
+    const numberOfQuotes=allQuotes.length;
+
+    // Pick a random number between 1 and the number of quotes
+    const randomNumber=mathLibrary.getRandomNumber(1,numberOfQuotes);
+
+    // Get the quote at that position in the array (minus one)
+    const selectedQuote=allQuotes[randomNumber-1];
+    // Print the quote to the console
+    console.log(selectedQuote);
+};
+
+// Function that loops indefinitely, calling the printAQuote function as it is 
+
+app.indefiniteLoop=function indefiniteLoop(){
+    setInterval(() => {
+        app.printAQuote();
+        
+    }, 1000);
+}
+
+
+app.indefiniteLoop();
