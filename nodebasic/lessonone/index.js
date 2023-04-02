94b549e3cea0e7dbbd49c15e01c6cc1fdbbd4801
shariful islam <sharifulb07@gmail.com>
2023-04-02 21:11:12 +0600

    user built module

---
+++ nodebasic/lessonone/index.js
@@ -0,0 +1,5 @@
+const {getAge}=require('./student');
+console.log(getAge())
+
+
+
---
+++ nodebasic/lessonone/student.js
@@ -0,0 +1,24 @@
+const getName=()=>{
+   return "Shariful Islam "; 
+}
+
+
+const getAge=()=>{
+    return "25"
+}
+
+const cgpa=4.50;
+
+module.exports={
+    getName, 
+    getAge,
+    cgpa
+}
+
+
+
+
+
+// exports.cgpa=cgpa;
+// exports.getName=getName;
+// exports.getAge=getAge;
