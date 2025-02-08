f28eb31eeb078d1b237f693e6a492ef44b90b2f2
sharifulb07 <sharifulb07@gmail.com>
2025-02-08 14:51:56 +0600

    events driven nodeje . here EventEmitter practice

--- app.js
+++ app.js
@@ -1,26 +1,60 @@
-const {writeFile, readFile } = require("fs").promises;
+
+const eventEmitter=require("events");
+
+const customEmitter=new eventEmitter();
+
+
+customEmitter.on("response",(name, id)=>{
+    console.log(`data recived user: ${name} with id ${id}`);
+    
+})
+customEmitter.on("response",()=>{
+    console.log("Some other logic here man ");
+    
+})
+
+customEmitter.emit("response", "Johnathon", 35)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {writeFile, readFile } = require("fs").promises;
 // const util=require('util');
 // const readFilePromise=util.promisify(readFile);
 // const writeFilePromise=util.promisify(writeFile);
 
 
-const start = async () => {
+// const start = async () => {
 
-    try {
-        const second = await readFile("./content/second.txt", 'utf-8');
-        const first = await readFile("./content/first.txt", 'utf-8');
+//     try {
+//         const second = await readFile("./content/second.txt", 'utf-8');
+//         const first = await readFile("./content/first.txt", 'utf-8');
 
-        await writeFile("./content/resultman.txt", `This is AWESOMEeeee text ${first} ${second} `, {flag:'a'})
+//         await writeFile("./content/resultman.txt", `This is AWESOMEeeee text ${first} ${second} `, {flag:'a'})
         
-        console.log(first, second);
-    } catch (error) {
-        console.log(error);
+//         console.log(first, second);
+//     } catch (error) {
+//         console.log(error);
         
-    }
+//     }
 
-};
+// };
 
-start();
+// start();
 
 
 
