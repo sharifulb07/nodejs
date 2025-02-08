8ec64e7051f94efa806549b38374550f190f3d2c
sharifulb07 <sharifulb07@gmail.com>
2025-02-08 13:23:44 +0600

    promises practice in different ways

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
-const _=require('lodash');
+const {writeFile, readFile } = require("fs").promises;
+// const util=require('util');
+// const readFilePromise=util.promisify(readFile);
+// const writeFilePromise=util.promisify(writeFile);
 
-const items=[1,[2,[3,[4,[5]]]]];
-const newItems=_.flattenDeep(items);
-console.log(newItems);
 
+const start = async () => {
+
+    try {
+        const second = await readFile("./content/second.txt", 'utf-8');
+        const first = await readFile("./content/first.txt", 'utf-8');
+
+        await writeFile("./content/resultman.txt", `This is AWESOMEeeee text ${first} ${second} `, {flag:'a'})
+        
+        console.log(first, second);
+    } catch (error) {
+        console.log(error);
+        
+    }
+
+};
+
+start();
+
+
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf-8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
