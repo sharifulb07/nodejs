108f7b697010f10fb079fdd33b2239dcae63d981
sharifulb07 <sharifulb07@gmail.com>
2025-11-07 15:56:37 +0600

    basic server concept

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const names=require("./day1/name")
+const http=require('http');
+const app=http.createServer((req, res)=>{
 
-const sayHey=(name)=>{
-    console.log(`Hello ${name}`)
-}
-sayHey(names.sharif)
+    if(req.url==="/"){
+        res.write("this is not fair man \n")
+        res.end("Heelo")
+    }
+})
+app.listen((5000),()=>{
+    console.log("hello world man")
+})
